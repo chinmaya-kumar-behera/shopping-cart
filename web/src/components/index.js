b87378a100a307baf89087ec82598ae1d1a5b0a1
Chinmaya Kumar Behera <official.chinu2127@gmail.com>
2024-06-19 05:09:23 +0530

    UI : added loading spinner

--- web/src/components/index.js
+++ web/src/components/index.js
@@ -2,4 +2,4 @@ export { default as PageContainer } from "./PageContainer";
 export { default as CartButton } from "./UI/CartButton";
 export { default as ProductButton } from "./UI/ProductButton";
 export { default as CategoryButton } from "./UI/CategoryButton";
-;
+export { default as Loader } from "./UI/Loader";
--- web/src/views/products/ProductContainer.jsx
+++ web/src/views/products/ProductContainer.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import ProductCard from './ProductCard';
+import React from "react";
+import ProductCard from "./ProductCard";
+import { Loader } from "../../components";
 
 const ProductContainer = ({ products }) => {
-    return (
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-        {products.map((el,ind) => (
-          <ProductCard key={ind} data={el} />
-        ))}
-      </div>
-    );
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+      {products.length === 0 ? (
+        <div className="p-10 flex justify-center">
+          <Loader />
+        </div>
+      ) : (
+        products.map((el, ind) => <ProductCard key={ind} data={el} />)
+      )}
+    </div>
+  );
 };
 
-export default ProductContainer
+export default ProductContainer;
