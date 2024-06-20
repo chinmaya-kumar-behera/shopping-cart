8a46953d769524a9e0dab6dc1c720e20e6a2573f
Chinmaya Kumar Behera <official.chinu2127@gmail.com>
2024-06-20 22:13:22 +0530

    initiated product order pages

--- web/src/pages/Order.js
+++ web/src/pages/Order.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ProductsHandler from '../handler/ProductsHandler';
+import { useSelector } from 'react-redux';
 
 const Order = () => {
+    const user = useSelector(state => state.auth.user);
+
+    const { getOrdersByUserIdHandler } = ProductsHandler();
+
+    useEffect(() => {
+        getOrdersByUserIdHandler({ userId: user._id })
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => console.log(err));
+    })
   return (
     <div>Order</div>
   )
